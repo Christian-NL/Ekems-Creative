e270b6219b8c26eff8754c9eb0f8d90554948ffe
ChristianNL <nsokengchristian@gmail.com>
2024-07-14 12:03:35 +0100

    nav on index

--- index.php
+++ index.php
@@ -39,44 +39,7 @@
 <body>
 
 <!-- NavBAr -->
-<nav class="navbar navbar-expand-lg rd-navbar-classic navbar-light bg-white fixed-top page-header-corporate">
-    <div class="container">
-        <div class="rd-navbar-panel d-flex justify-content-between align-items-center w-100">
-            <!-- Bouton de bascule pour la navigation responsive -->
-            <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="navbar-brand mx-auto">
-                <h4 class="font-weight-light font-weight-bold mb-0">Ekem's Creative</h4>
-            </div>
-        </div>
-
-        <!-- Contenu de la navigation -->
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder active" href="index.html">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder" href="produits.php">Produits</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder" href="services.php">Services</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder" href="about_us.php">À Propos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder" href="contact_us.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link font-weight-bolder" href="realisation.php">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php include_once "config/get_nav.php" ; ?>
 
 
 <section class="section bg-img-1 my-5 mt-5">
